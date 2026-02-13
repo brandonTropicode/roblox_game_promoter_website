b6456d5df51e260e9ec973374ffa76e660b8695c
Brandon <brandon@Boriss-Mac-mini.local>
2026-02-13 17:32:04 -0500

    UPDATE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,23 @@ import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import Home from "./pages/Home";
 import AdminLogin from "./pages/AdminLogin";
 import AdminHome from "./pages/AdminHome";
+import ProtectedRoute from "./pages/ProtectedRoute"
 
 function App() {
+  const admin_login = localStorage.getItem("admin_login") === "true";
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin-login" element={<AdminLogin />} />
-        <Route path="/admin" element={<AdminHome />} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute isallowed={admin_login} redirectPath="/admin-login">
+              <AdminHome />
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
