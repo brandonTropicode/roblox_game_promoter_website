8f4bff2225d9f71f4ecb37a3b33b8cda242db5e6
Brandon <brandon@Boriss-Mac-mini.local>
2026-02-13 17:24:44 -0500

    UPDATE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,19 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
-import AdminLogin from './pages/AdminLogin';
-import AdminHome from './pages/AdminHome';
-import ProtectedRoute from './pages/ProtectedRoute';
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import AdminLogin from "./pages/AdminLogin";
+import AdminHome from "./pages/AdminHome";
 
 function App() {
-  const admin_login = localStorage.getItem("admin_login")
   return (
-    <Router basename={import.meta.env.BASE_URL}>
+    <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/admin-login' element={<AdminLogin />} />
-        {/* <Route path='/admin' element={<ProtectedRoute isallowed = {admin_login} redirectPath={"admin-login"}><AdminHome/></ProtectedRoute>} /> */}
-        <Route path='/admin' element={<AdminHome />} />
-        <Route path='*' element={<Navigate to="/" replace />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/admin" element={<AdminHome />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
